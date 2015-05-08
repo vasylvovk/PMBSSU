5583b98d8085d1166d1e2ab1711c61f02e48dae9
vasyl <vasyl@debian>
2015-05-09 00:08:11 +0300

    Initial commit

---
+++ src/main/java/com/gmail/vasylvovkast/ht01/HT01.java
@@ -0,0 +1,21 @@
+package com.gmail.vasylvovkast.ht01;
+
+/**
+ * @author Vasyl Vovk
+ * Завдання в якому потрібно вивести на екран всі непарні числа числа у
+ * діапазоні [0..10].
+ *
+ */
+public class HT01
+{
+    public static void main( String... args )
+    {
+        int N = Integer.decode(args[0]);
+        int odd = 1;
+        while (odd <= N) {
+            System.out.print(odd + "\t");
+            odd+=2;
+        }
+        System.out.println();
+    }
+}
